355a2f069fb3fc022d09336a8b309f6aa841e731
Joao_Antonio <ja.alvesgoes@gmail.com>
2025-09-14 23:57:38 -0300

    Mensagem sobre a alteração no db.js

--- backend/db.js
+++ backend/db.js
@@ -12,20 +12,3 @@ const pool = new Pool({
 module.exports = {
     query: (text, params) => pool.query(text, params),
 };
-
-/*
-const { Pool } = require('pg'); // 1. Importa a classe Pool da biblioteca 'pg'
-
-const pool = new Pool({ // 2. Cria uma instância da conexão (pool de conexões)
-    user: 'postgres', // 3. Usuário do banco de dados
-    host: 'localhost', // 4. Onde o banco de dados está rodando
-    database: 'faculdade_db', // 5. Nome do banco de dados que será usado
-    password: 'JOAOdaufrn2025.1', // 6. Senha do usuário 'postgres'
-    port: 5432, // 7. Porta padrão do PostgreSQL
-});
-
-module.exports = {
-    // 8. Exporta uma função que executa consultas SQL de forma segura
-    query: (text, params) => pool.query(text, params),
-};
-*/
